2829d8942e9736629cda7a81872bbe1c96013490
peachhike <peachhike@naver.com>
2025-03-14 15:47:20 +0900

    feat/refactor:thirst

--- Assets/Scripts/Player/PlayerCondition.cs
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -20,6 +20,7 @@ public class PlayerCondition : MonoBehaviour, IDamageable
     private void Update()
     {
         hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        thirst.Subtract(thirst.passiveValue * Time.deltaTime);
         stamina.Add(stamina.passiveValue * Time.deltaTime);
 
         if (hunger.curValue == 0f)
