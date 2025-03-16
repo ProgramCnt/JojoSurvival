cad269b028464de613a54ac26f9d2433576b14d7
ProgramCnt <corfafa2@naver.com>
2025-03-17 00:32:43 +0900

    feat : 아이템 장착 및 동작 구현

--- Assets/Scripts/Item/Entity.cs
+++ Assets/Scripts/Item/Entity.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour, IEntity
 {
-    private Condition _health;
-    public Condition health { get { return _health; } }
+    public int curHealth;
+    public int maxHealth;
 
     public ItemData dropItem;
 
     // Start is called before the first frame update
     void Start()
     {
-        _health.curValue = _health.maxValue;
+        curHealth = maxHealth;
     }
 
     public void OnTakeDamage(int damage)
     {
         Instantiate(dropItem.dropPrefab, transform.position, Quaternion.identity);
-        _health.Subtract(damage);
-        if (health.curValue <= 0)
+        curHealth -= damage;
+        if (curHealth <= 0)
         {
             Destroy(gameObject);
         }
