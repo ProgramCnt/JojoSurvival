1b00486a88586087bfad6371496d2e776bd4281c
leeejinho <86703795+leeejinho@users.noreply.github.com>
2025-03-17 15:18:45 +0900

    feat: 크래프트 스크립트 추가

---
+++ Assets/Scripts/Craft/CraftingManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingManager : MonoBehaviour
+{
+    public List<CraftingRecipe> recipes;
+    public Inventory inventory;
+    
+    public void ShowCraftList()
+    {
+        
+    }
+
+    public void TryCraft()
+    {
+
+    }
+}
